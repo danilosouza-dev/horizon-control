772eeb16c1b02b9b5b868aefbd073b0e98f72842
Danilo Souza <gtd.danilosouza@gmail.com>
2022-04-09 18:14:01 -0300

    feat: adicionado react-router-dom 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter } from 'react-router-dom'
+import { AppRouter } from './routes'
 
 export function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export { };
+import { Navigate, Route, Routes } from "react-router-dom";
+
+export function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<p>Pagina inicial</p>}/>
+      <Route path="*"element={<Navigate to="/pagina-inicial"/>} />
+    </Routes>
+  )
+}
